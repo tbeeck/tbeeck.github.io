6b9dd8670c40745da80e03890463350da26bd370
Tim Beck <socaltdb@gmail.com>
2020-06-13 09:08:48 -0700

    add new index and files

---
+++ assets/js/credits.js
@@ -0,0 +1,16 @@
+
+/* Fancy scrolling adadpted from
+https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link*/
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+/* Trigger animation when in viewport */
+$("#thingsILike").inviewport({
+  threshold: 75,
+  className: 'animate-logos'
+});
